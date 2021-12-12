cf37d76ad577c135130934a316fa9bf0a93172a3
Maxime Bonnin <maxime.bonnin@gmx.de>
2021-12-12 13:42:30 +0100

    improved readability

--- main.py
+++ main.py
@@ -1,37 +1,60 @@
-# Import
+# ------------------- Import -------------------
+
 import pygame
 import math
 import random
 import os
 
 
-# Global Variables
+# ------------------- Global Variables -------------------
+
 WIDTH, HEIGHT = 500, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
-COLORS = {
-    "black" : (0, 0, 0),
-    "white" : (255, 255, 255)
 
+COLORS = {
+    "black": (0, 0, 0),
+    "white": (255, 255, 255),
+    "red": (255, 255, 204),
+    "yellow": (255, 255, 0),
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255)
 }
 
 
-# Initiallize
+# ------------------- Initiallize -------------------
+
 pygame.init
 pygame.display.set_caption("Tower Defence Game")
 
 
-# Functions
+# ------------------- CLASSES -------------------
+
+class Tower:
+    # Class that describes towers
+    def __init__(self):
+        pass
+
+class Unit:
+    # Class that describes units
+    def __init__(self):
+        pass
+
+
+# ------------------- FUNCTIONS -------------------
 
 def draw_window():
+    # Function that draws everything on screen (every frame)
+
     WIN.fill(COLORS["white"])
     pygame.display.update()
 
 
-# Main Game Loop
+# ------------------- Main Game Loop -------------------
 
 def main():
     clock = pygame.time.Clock()
+
     run = True
     while run:
         clock.tick(FPS)
@@ -44,6 +67,6 @@ def main():
     pygame.quit()
 
 
-# Save for multiple files being executed
+# ------------------- Save for multiple files being executed -------------------
 if __name__ == "__main__":
     main()
